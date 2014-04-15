a8e72e387ddd2a14c98c78d097fc99673ae47bfb
wim82 <wimmaesschalck@gmail.com>
2014-04-15 14:02:34 +0200

    Optimized EditReservationView + added delete button, implemented check on user

--- Cronos Rooms/Model/Reservation.m
+++ Cronos Rooms/Model/Reservation.m
@@ -10,22 +10,6 @@
 @implementation Reservation {
 
 }
-/*
-+ (NSDateFormatter *)dateFormatter {
- static NSDateFormatter *dateFormatter = nil;
-
- if (dateFormatter == nil) {
-     dateFormatter = [[NSDateFormatter alloc] init];
-     [dateFormatter setDateFormat:@"yyyyMMdd"];
- }
-
- return dateFormatter;
-
-}
-  */
-
-
-
 
 - (instancetype)initWithStringDictionary:(NSDictionary *)dictionary {
     if (self) {
@@ -34,26 +18,26 @@
         self.meetingRoom = [[MeetingRoom alloc] initWithDictionary:[dictionary objectForKey:@"meetingRoom"]];
         self.startTime = [[DateHelper datetimeFormatter] dateFromString:[dictionary objectForKey:@"startTime"]];
         self.endTime = [[DateHelper datetimeFormatter] dateFromString:[dictionary objectForKey:@"endTime"]];
-       // NSLog(@"endtime in in init %@", self.endTime);
+        // NSLog(@"endtime in in init %@", self.endTime);
         self.reservationDescription = [dictionary objectForKey:@"description"];
     }
     return self;
 }
 
 
--(NSMutableDictionary *)convertToDictionary {
-    
-    NSMutableDictionary *reservationDictionary = [[NSMutableDictionary alloc]init];
-    
-    [reservationDictionary setValue: [NSNumber numberWithInt:self.reservationId] forKey:@"id"];
+- (NSMutableDictionary *)convertToDictionary {
+
+    NSMutableDictionary *reservationDictionary = [[NSMutableDictionary alloc] init];
+
+    [reservationDictionary setValue:[NSNumber numberWithInt:self.reservationId] forKey:@"id"];
     [reservationDictionary setObject:[self.user convertToDictionary] forKey:@"user"];
     [reservationDictionary setObject:[self.meetingRoom convertToDictionary] forKey:@"meetingRoom"];
     [reservationDictionary setObject:[self createStringFromDateTime:self.startTime] forKey:@"startTime"];
     [reservationDictionary setObject:[self createStringFromDateTime:self.endTime] forKey:@"endTime"];
     [reservationDictionary setObject:self.reservationDescription forKey:@"description"];
-    
+
     NSLog(@"update dictionary: %@", [reservationDictionary objectForKey:@"id"]);
-    
+
     return reservationDictionary;
 }
 
@@ -61,15 +45,15 @@
     NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
     [dateFormatter setDateFormat:@"yyyyMMdd HH:mm"];
     return
-[dateFormatter stringFromDate:date];
+            [dateFormatter stringFromDate:date];
 }
 
 
 + (NSMutableArray *)sortByStartTime:(NSMutableArray *)reservations {
     NSArray *sortedArray;
     sortedArray = [reservations sortedArrayUsingComparator:^NSComparisonResult(id a, id b) {
-        NSDate *first = [(Reservation*)a startTime];
-        NSDate *second = [(Reservation*)b startTime];
+        NSDate *first = [(Reservation *) a startTime];
+        NSDate *second = [(Reservation *) b startTime];
         return [first compare:second];
     }];
 
