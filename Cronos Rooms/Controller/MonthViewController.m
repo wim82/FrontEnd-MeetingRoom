ae49035d5385cf57122f5cb2de09f089b980664b
wim82 <wimmaesschalck@gmail.com>
2014-04-16 10:42:50 +0200

    SettingsViewController update

--- Cronos Rooms/Controller/MonthViewController.m
+++ Cronos Rooms/Controller/MonthViewController.m
@@ -54,36 +54,36 @@ NSDate * today;
 
 
 - (void)loadView{
-    
+
     self.viewMonthOverview = [[MonthOverview alloc]initWithFrame:[UIScreen mainScreen].bounds andDelegate:self];
     self.view = self.viewMonthOverview;
-    
-    
 
-    
+
+
+
     //TODO #define number of days to prefill in the calendar (back in history/days before today) And number of months in calendar
     NSInteger xNumberOfDays=-10;
     NSInteger numberOfMonths=11;
     NSDate *date=[[NSDate alloc]init]; //vandaag
-    
+
     startDate = [self fillWithXNumberOfDays:date: xNumberOfDays];
-    
-    
+
+
     monthsAndDaysDictionary=[[NSMutableDictionary alloc]init];
     calendar = [NSCalendar autoupdatingCurrentCalendar];
     keyArray = [[NSMutableArray alloc]init]; //months in Array, starting with first month corresponding with startDate
-    
+
     date=startDate;
     NSLog(@"date is : %@", date);
-    
+
     //volgende dag
     NSDate *nextDate = [self fillWithXNumberOfDays:date :1];
 
-    
+
     daysInMonthArray=[[NSMutableArray alloc]init]; //array of all the days in a Month, including the fake dates
     for (int i=1; i<=numberOfMonths; i++){
         [keyArray addObject:[self displayStringMonthFromDate:date]];  //fill keyArray with corresponding months of the dictionary
-        
+
         //check if first of month is a Monday or not. add fake date cell to beginning of the month to start from Monday column (note: Sunday=1)
         int dayOfWeek = [[[NSCalendar currentCalendar] components:NSWeekdayCalendarUnit fromDate:date] weekday];
         NSLog(@"dayofweek : %d", dayOfWeek);
@@ -112,22 +112,22 @@ NSDate * today;
             for (int j=0; j<5; j++){
                 [daysInMonthArray addObject:[self fakeDate]];
             }}
-        
-        
+
+
         [daysInMonthArray addObject:date];
- 
-  
+
+
         while ([[self displayStringMonthFromDate:date] isEqualToString:[self displayStringMonthFromDate:nextDate]]){
             [daysInMonthArray addObject:nextDate];
             date=nextDate;
             nextDate = [self fillWithXNumberOfDays:date :1];
             }
-       
+
         [monthsAndDaysDictionary setObject:daysInMonthArray forKey:[self displayStringMonthFromDate:date]];
         date=nextDate;
         nextDate = [self fillWithXNumberOfDays:date :1];
         daysInMonthArray = [[NSMutableArray alloc]init];
- 
+
     }
     }
 
@@ -136,21 +136,21 @@ NSDate * today;
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
+
     self.title = self.meetingRoom.roomName;
-    
+
     [self.viewMonthOverview.collectionView registerClass:[MonthCell class] forCellWithReuseIdentifier:CELL_IDENTIFIER_MONTHDAY];
-    
+
     [self.viewMonthOverview.collectionView registerClass:[MonthHeader class] forSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:HEADER_IDENTIFIER_MONTH];
-    
-    
+
+
    //load data from database: reservations per meetingroom and public holidays
-    
+
     [self _loadPublicHolidays];
     NSLog(@"startDate %@", startDate);
     [self _loadReservations: roomId : startDate : 450];
 
-    
+
 }
 
 - (void)didReceiveMemoryWarning
@@ -171,14 +171,14 @@ NSDate * today;
 }
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
-    
+
     return [[monthsAndDaysDictionary objectForKey:[keyArray objectAtIndex:section]] count];
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
-    
-    
-    
+
+
+
     MonthCell * cell = [collectionView dequeueReusableCellWithReuseIdentifier:CELL_IDENTIFIER_MONTHDAY forIndexPath:indexPath];
 
     cell.lblName.text=@"";
@@ -188,25 +188,25 @@ NSDate * today;
     dayInArray=[[monthsAndDaysDictionary objectForKey:[keyArray objectAtIndex:indexPath.section]] objectAtIndex:indexPath.row] ;
     NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
     [dateFormat setDateFormat:@"yyyy-MM-dd"];
-    
+
     if ([[dateFormat stringFromDate:dayInArray] isEqualToString:[dateFormat stringFromDate:today]])
     {
         cell.backgroundColor=[UIColor app_blueGrey];
     }
-    
+
     NSString * dayContent  = [[NSString alloc]init];
     for(int i=0;i<[self.reservationsPerRoom count]; i++){
         Reservation *reservation = [self.reservationsPerRoom objectAtIndex:i];
-        
+
         if ([[dateFormat stringFromDate:dayInArray] isEqualToString:[dateFormat stringFromDate:reservation.startTime]])
         {
             cell.backgroundColor=[UIColor app_lightYellow];
             dayContent=[dayContent stringByAppendingString:reservation.reservationDescription];
             dayContent=[dayContent stringByAppendingString:@"\n"] ;
-            
+
         }}
-    
-    
+
+
     for(int i=0;i<[self.publicHolidays count]; i++){
         PublicHoliday *publicHoliday = [self.publicHolidays objectAtIndex:i];
         if ([[dateFormat stringFromDate:dayInArray] isEqualToString:[dateFormat stringFromDate:publicHoliday.holidayDate]])
@@ -220,44 +220,44 @@ NSDate * today;
     if([[AppState sharedInstance] deviceIsiPad]){
         cell.lblReservations.text = dayContent;
     }
-    
-    
+
+
     //remove the fakeCells TODO: make the fakecells inactive
     NSDateComponents *components = [calendar components:NSYearCalendarUnit
                                     | NSMonthCalendarUnit | NSDayCalendarUnit fromDate:dayInArray];
     if (components.year !=1970){
         cell.lblName.text = [NSString stringWithFormat:@"%d",components.day];
-      
+
     }
     else{
         cell.backgroundColor=[UIColor clearColor];
-        
+
     }
-    
-    
-    
-    
+
+
+
+
     [self gestureRecognition:cell:indexPath.row];
-    
+
     return cell;
 }
 
 
 
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout referenceSizeForHeaderInSection:(NSInteger)section {
-    
+
     return CGSizeMake(0, 50);
-    
+
 }
 
 - (UICollectionReusableView *)collectionView: (UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath {
     MonthHeader *headerView = [collectionView dequeueReusableSupplementaryViewOfKind:
                                UICollectionElementKindSectionHeader withReuseIdentifier:HEADER_IDENTIFIER_MONTH forIndexPath:indexPath];
 
-    
+
   //  NSString *headerText =  [months objectForKey:[keyArray objectAtIndex:indexPath.section]] ;
     NSString *headerText =  [keyArray objectAtIndex:indexPath.section] ;
-    
+
     headerView.lblHeader.text= headerText;
     return headerView;
 }
@@ -267,19 +267,19 @@ NSDate * today;
 - (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
 {
     // TODO: Select Item
-    
+
     DayViewController *dayViewController = [[DayViewController alloc] init];
     dayViewController.meetingRoom =self.meetingRoom;
     NSDate *date=[[NSDate alloc]init];
     date=[[monthsAndDaysDictionary objectForKey:[keyArray objectAtIndex:indexPath.section]] objectAtIndex:indexPath.row] ;
     dayViewController.date=date;
-    
-    
+
+
     NSLog (@"roomId %ld",(long)roomId);
     NSLog(@"date %@", date);
 
     [self.navigationController pushViewController:dayViewController animated:YES];
-   
+
 }
 - (void)collectionView:(UICollectionView *)collectionView didDeselectItemAtIndexPath:(NSIndexPath *)indexPath {
     // TODO: Deselect item
@@ -293,7 +293,7 @@ NSDate * today;
     //int width = screen.currentMode.size.width;
     int height = screen.currentMode.size.height;
     CGFloat width = CGRectGetWidth(self.viewMonthOverview.bounds);
-    
+
     return CGSizeMake(width/8,100);
 }
 
@@ -302,7 +302,7 @@ NSDate * today;
 
 - (UIEdgeInsets)collectionView:
 (UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout insetForSectionAtIndex:(NSInteger)section {
-    
+
     return UIEdgeInsetsMake(50, 20,50, 20);
 }
 
@@ -310,7 +310,7 @@ NSDate * today;
 
 //this adds tap and long press gesture recognizer to the cell with number indexpath.row
 -(void) gestureRecognition:(MonthCell *) cell : (NSInteger) row {
-    
+
     cell.tag = row;
 /*
     // Press/tap
@@ -326,22 +326,22 @@ NSDate * today;
     lpgr.delegate=self;
     [cell addGestureRecognizer:lpgr];
 
-    
+
 }
 
 -(void)cellTapped:(id)sender{
     NSLog(@"Cell Tapped");
-    
-    
-    
+
+
+
 }
 
 - (IBAction)handleLongPress:(UILongPressGestureRecognizer *)gestureRecognizer
 {
     if (gestureRecognizer.state == UIGestureRecognizerStateBegan) {
-        
+
         CGPoint p = [gestureRecognizer locationInView:self.viewMonthOverview.collectionView];
-        
+
         NSIndexPath *indexPath = [self.viewMonthOverview.collectionView indexPathForItemAtPoint:p];
         if (indexPath == nil) {
             NSLog(@"long press on table view but not on a row");
@@ -350,7 +350,7 @@ NSDate * today;
             if (cell.isHighlighted) {
                 NSLog(@"long press on table view at section %d row %d", indexPath.section, indexPath.row);
                 NSIndexPath *cellIndexPath = [self.viewMonthOverview.collectionView indexPathForCell:cell];
-                
+
                 NSDate *date=[[NSDate alloc]init];
                 date=[[monthsAndDaysDictionary objectForKey:[keyArray objectAtIndex:cellIndexPath.section]] objectAtIndex:cellIndexPath.row] ;
                 Reservation *reservation = [[Reservation alloc] init];
@@ -359,8 +359,8 @@ NSDate * today;
                 EditReservationViewController *editReservationViewController = [[EditReservationViewController alloc] init];
                 editReservationViewController.reservation = reservation;
                 [self.navigationController pushViewController:editReservationViewController animated:YES];
-                
-                
+
+
             }
         }
     }
@@ -370,11 +370,11 @@ NSDate * today;
 #pragma mark - access DataBase
 
 - (void) _loadPublicHolidays {
-    
+
 [[PublicHolidayService sharedService] getAllPublicHolidaysWithSuccessHandler:^(NSMutableArray *publicHolidays) {
     self.publicHolidays = [[NSArray alloc] initWithArray:publicHolidays];
     [self.viewMonthOverview .collectionView reloadData];
-    
+
 } andErrorHandler:^(NSException * exception) {
     UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error" message:exception.reason delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
     [alertView show];
@@ -389,7 +389,7 @@ NSDate * today;
       UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error" message:exception.reason delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
       [alertView show];
   }];
-    
+
 }
 
 #pragma marks - date calculators
@@ -402,7 +402,7 @@ NSDate * today;
     NSDate * date=[[NSCalendar currentCalendar] dateFromComponents:components];
     NSLog (@" today : %@, and date : %@",today, date);
     return date;
-    
+
 }
 
 + (NSDate *)dateWithOutTime:(NSDate *)date {
@@ -418,16 +418,16 @@ NSDate * today;
     NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
     [dateFormatter setDateFormat:@"MMMM"];
     return [dateFormatter stringFromDate:date];
-    
-    
+
+
 }
 
 - (NSString *)displayStringDayFromDate:(NSDate *)date {
     NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
     [dateFormatter setDateFormat:@"EEEE"];
     return [dateFormatter stringFromDate:date];
-    
-    
+
+
 }
 
 //fake date
@@ -437,7 +437,7 @@ NSDateComponents *fakeComponents=[[NSDateComponents alloc]init];
 [fakeComponents setMonth:1];
 [fakeComponents setYear:1970];
     return [[NSCalendar currentCalendar] dateFromComponents:fakeComponents] ;
-   
+
 }
 
 @end
--- Cronos Rooms/View/Setttings/SettingsView.m
+++ Cronos Rooms/View/Setttings/SettingsView.m
@@ -17,18 +17,22 @@
 
     if (self) {
 
-        self.backgroundColor = [UIColor app_blueGreyShaded];
+        self.backgroundColor = [UIColor app_darkGrey];
         self.userTitle = [[GroupTitleView alloc] initWithFrame:CGRectMake(0, 128, frame.size.width, DIMENSIONS_GROUP_TITLE_VIEW_HEIGHT)
-                                                      andTitle:@"Details"];
+                                                      andTitle:@"Settings"];
         [self addSubview:self.userTitle];
 
 
         self.userNameDetail = [[DetailCellView alloc] initWithFrame:CGRectMake(0, self.userTitle.frame.origin.y + self.userTitle.frame.size.height, frame.size.width, 64)
-                                                              title:@"Fullname"
+                                                              title:@"Name"
                                                            andValue:nil
                                                         andDelegate:self];
         self.userNameDetail.backgroundColor = [UIColor whiteColor];
         [self addSubview:self.userNameDetail];
+
+        self.saveButton = [[UIButton alloc] initWithFrame:CGRectMake(frame.size.width / 100 *20, self.userNameDetail.frame.origin.y + self.userNameDetail.frame.size.height + 40, frame.size.width / 100 * 60, 44)];
+        [self.saveButton setTitle:@"Save" forState:UIControlStateNormal];
+        [self addSubview:self.saveButton];
 
 
     }
